9e2d150f7b6e71a61fe80aff9ad343868f43c209
Katzu <alvaro.valerar@gmail.com>
2025-11-26 20:47:58 -0500

    Cargar imagen en ventana

--- test.js
+++ test.js
@@ -1,39 +1,25 @@
-// 1. Configuración del Juego
+class SceneA extends Phaser.Scene {
+    constructor() {
+        super('sceneA');
+    }
+
+    preload() {
+        this.load.image('personaje', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3ihZp9K4lYec2DlsMRqBQoAl6Hn3eMMC7_A&s'); 
+    }
+
+    create() {
+        const imagen = this.add.image(200, 400, 'personaje');
+    }
+}
+
 const config = {
-    // 1.1 Tipo de renderizado (AUTO intentará usar WebGL, si no, usará Canvas)
+
     type: Phaser.AUTO,
-    // 1.2 Dimensiones del "lienzo" (Canvas) de tu juego
+
     width: 800,
     height: 600,
-    // 1.3 Elemento HTML para inyectar el Canvas (opcional, si no se especifica, lo añade al <body>)
-    // parent: 'game-container',
-    // 1.4 Escenas del juego
-    scene: {
-        preload: preload, // Carga de assets
-        create: create,   // Inicialización de objetos
-        update: update    // Lógica del juego (bucle principal)
-    }
+
+    scene: [SceneA]
 };
 
-// 2. Creación de la Instancia de Phaser.Game
-// ¡Esto es lo que crea el "lienzo" (Canvas) en tu página HTML!
 const game = new Phaser.Game(config);
-
-// 3. Funciones de las Escenas
-
-// Función para cargar imágenes, sonidos, etc.
-function preload ()
-{
-    this.load.image('T1TEAM', 'image/assets/T1TEAM.jpg');
-}
-
-// Función que se ejecuta una vez, después de la carga
-function create ()
-{
-    this.add.image(400, 300, 'T1TEAM');
-}
-
-// Función que se ejecuta en cada frame
-function update (time, delta)
-{
-}
